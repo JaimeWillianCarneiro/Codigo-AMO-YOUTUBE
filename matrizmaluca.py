1eca6a22d81910848b21c934643c7694ad59cf8e
Jaime Willian <104590276+JaimeWillianCarneiro@users.noreply.github.com>
2023-10-31 13:48:48 -0300

    Update matrizmaluca.py

--- matrizmaluca.py
+++ matrizmaluca.py
@@ -1,31 +1,20 @@
 n, m , p = [int(i) for i in input().split()]
 
 
-
-
-
-
 matriz  = [[int(i) for i in input().split()] for _ in range(n)]
-
-
-
 
 pontosEnzo = 0 
 pontosLobo = 0 
 
-
-
 for partida in range(1, p+1):
     pontos = 0 
-    
-    
-    
+       
     l, c = map(int, input().split())
     l-=1
     c-=1
     
     
-    # SOmando os valores da linha
+    # Somando os valores da linha
     for i in range(m):
         
         pontos+= matriz[l][i]
@@ -41,7 +30,7 @@ for partida in range(1, p+1):
         pontosEnzo+=pontos
     else:
         pontosLobo+=pontos
-        
+      
         
 
 if pontosEnzo > pontosLobo:
